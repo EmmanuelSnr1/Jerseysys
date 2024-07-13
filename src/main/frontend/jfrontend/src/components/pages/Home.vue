fdfbe4b1f920800a9a2dfc3fdf6db7f637aca224
Emmanuel Snr <emmanuelappiah124@gmail.com>
2024-07-13 21:44:20 +0100

    Frontend Consolidated

--- src/main/frontend/jfrontend/src/components/pages/Home.vue
+++ src/main/frontend/jfrontend/src/components/pages/Home.vue
@@ -1,5 +1,16 @@
 <template>
     <Navbar/>
+
+    <main class="p-4 h-auto mx-auto max-w-screen-lg">
+
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span
+          class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Better Data</span> Scalable
+        AI.</h1>
+      <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets
+        where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+    </main>
+
+
     <Footer/>
 </template>
   
@@ -12,4 +23,3 @@ import Footer from '../layout/Footer.vue';
 <style scoped>
 /* Add any additional styling here */
 </style>
-  
--- src/main/frontend/jfrontend/vite.config.js
+++ src/main/frontend/jfrontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8443', // Replace with your backend server URL
+        target: 'http://localhost:8080', // Replace with your backend server URL
         changeOrigin: true,
         // Optionally, rewrite paths if needed:
         // rewrite: (path) => path.replace(/^\/api/, ''),
